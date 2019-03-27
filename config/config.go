3b9ffe23535b90b49a82b8ecead2c2d579f3a440
Sandeep Dasika <dasika.sandy@gmail.com>
2019-03-27 16:38:40 +0530

    Connect calls to node are now blocking

--- config/config.go
+++ config/config.go
@@ -20,8 +20,14 @@ var MinIndexFiles int32 = 5
 // DownloadTimeout : The timeout after which file download errors out.
 var DownloadTimeout = 5 * time.Minute
 
+// SearchTimeout : Duration after which the search call times out.
+var SearchTimeout = 3 * time.Second
+
 // ChunkBufSize : The buffer size required for a single chunk of a file.
 var ChunkBufSize = 1024 * 512
 
 // TrackerConnectTimeout : Duration after which client connection to tracker times out.
 var TrackerConnectTimeout = 5 * time.Second
+
+// NodeConnectTimeout : Duration after which client connection to Node times out.
+var NodeConnectTimeout = 5 * time.Second
