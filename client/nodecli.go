ab8749fc33d0ec0e5e9bb898fc198f020a7a740a
Sandeep Dasika <dasika.sandy@gmail.com>
2019-04-02 11:51:48 +0530

    Separating search in nodecli.go to usersearch.go

--- client/nodecli.go
+++ client/nodecli.go
@@ -4,19 +4,13 @@ import (
 	"fmt"
 	"log"
 
-	"github.com/gravetii/diztl/diztl"
 	pb "github.com/gravetii/diztl/diztl"
 )
-
-type searchResult struct {
-	node *diztl.Node
-	file *diztl.FileMetadata
-}
 
 // UserCLI : Starts a for{} to take user inputs for file search.
 func UserCLI() {
 	for {
-		in, ok := searchInput()
+		in, ok := input()
 		if !ok {
 			break
 		}
@@ -32,18 +26,6 @@ func UserCLI() {
 	}
 }
 
-func search(in string) ([]*searchResult, bool) {
-	fmt.Printf("Performing search for string: %s\n", in)
-	responses, err := nodeclient.Search(in)
-	if err != nil {
-		log.Printf("Could not search due to an error: %v\n", err)
-		fmt.Println("Unable to search the network at this moment.")
-		return nil, false
-	}
-
-	return validateResponses(responses)
-}
-
 func display(res []*searchResult) {
 	fmt.Printf("\n%30s | %30s", "Option", "File Name\n")
 	for c, r := range res {
@@ -51,7 +33,7 @@ func display(res []*searchResult) {
 	}
 }
 
-func searchInput() (string, bool) {
+func input() (string, bool) {
 	var pattern string
 	fmt.Printf("\n\n***************  DIZTL  ***************\n\n")
 	fmt.Printf("Enter a pattern to search for. '*' to Exit - ")
@@ -75,20 +57,4 @@ func download(res []*searchResult) {
 			log.Printf("Finished downloading file: %s\n", f.Name())
 		}
 	}
-}
-
-func validateResponses(responses []*diztl.SearchResp) ([]*searchResult, bool) {
-	if len(responses) == 0 {
-		fmt.Printf("No files with the given name were found in the network. Try another search!")
-		return nil, false
-	}
-
-	r := []*searchResult{}
-	for _, resp := range responses {
-		for _, file := range resp.GetFiles() {
-			r = append(r, &searchResult{resp.GetNode(), file})
-		}
-	}
-
-	return r, true
 }
---
+++ client/usersearch.go
@@ -0,0 +1,41 @@
+package client
+
+import (
+	"fmt"
+	"log"
+
+	"github.com/gravetii/diztl/diztl"
+)
+
+type searchResult struct {
+	node *diztl.Node
+	file *diztl.FileMetadata
+}
+
+func search(input string) ([]*searchResult, bool) {
+	fmt.Printf("Performing search for string: %s\n", input)
+	responses, err := nodeclient.Search(input)
+	if err != nil {
+		log.Printf("Could not search due to an error: %v\n", err)
+		fmt.Println("Unable to search the network at this moment.")
+		return nil, false
+	}
+
+	return validateResponses(responses)
+}
+
+func validateResponses(responses []*diztl.SearchResp) ([]*searchResult, bool) {
+	if len(responses) == 0 {
+		fmt.Printf("No files with the given name were found in the network. Try another search!")
+		return nil, false
+	}
+
+	r := []*searchResult{}
+	for _, resp := range responses {
+		for _, file := range resp.GetFiles() {
+			r = append(r, &searchResult{resp.GetNode(), file})
+		}
+	}
+
+	return r, true
+}
