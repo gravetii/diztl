b7390cb4bcbe079f94b206f5843b528862717438
Sandeep Dasika <dasika.sandy@gmail.com>
2019-06-17 20:45:29 +0530

    Better way of creating temp files in reader and writer

--- dir/dir.go
+++ dir/dir.go
@@ -29,36 +29,6 @@ func GetOutputPath(fname string) (string, error) {
 	return filepath.Join(conf.OutputDir(), fname), nil
 }
 
-// GetTempPathForUpload returns the path to the file in the system's temp directory.
-// This is the location where the client sending a file to its peer copies the file
-// from the source folder before sending it in chunks to the receiver node.
-func GetTempPathForUpload(fname string) (string, error) {
-	return getTempPath(fname, "u")
-}
-
-// GetTempPathForDownload returns the path to the file in the system's temp directory.
-// This is the location where the file is initially downloaded and formed from the chunks sent
-// by the source node. After download, it's then moved to the user-configured output folder.
-func GetTempPathForDownload(fname string) (string, error) {
-	return getTempPath(fname, "d")
-}
-
-func getTempPath(fname string, prefix string) (string, error) {
-	fpath := filepath.Join(os.TempDir(), prefix)
-	if err := ensure(fpath); err != nil {
-		return "", errors.New("Could not ensure required temp directories exist - " + err.Error())
-	}
-
-	return filepath.Join(fpath, fname), nil
-}
-
-// GetTempPath returns the path to the file in the system's temp directory.
-// This is where the file is first created on the receiver node when download
-// is initiated and is later moved to the output folder when download completes.
-func GetTempPath(fname string) string {
-	return filepath.Join(os.TempDir(), fname)
-}
-
 // GetLogPath returns the path to the logger file or an error if any.
 func GetLogPath(fname string) (string, error) {
 	if err := ensure(conf.AppDir()); err != nil {
