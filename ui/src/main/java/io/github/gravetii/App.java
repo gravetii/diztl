0daf422225cffa17ed3bb0da4b080f7b18c6527c
Sandeep Dasika <dasika.sandy@gmail.com>
2019-07-21 09:03:01 +0530

    Moving to AnchorPane for share-dirs scene

--- ui/src/main/java/io/github/gravetii/App.java
+++ ui/src/main/java/io/github/gravetii/App.java
@@ -3,6 +3,7 @@ package io.github.gravetii;
 import io.github.gravetii.client.connection.CommunicationClient;
 import io.github.gravetii.scene.FxScene;
 import io.github.gravetii.scene.start.StartScene;
+import io.github.gravetii.scene.userdir.UserDirsScene;
 import javafx.application.Application;
 import javafx.stage.Stage;
 import org.slf4j.Logger;
