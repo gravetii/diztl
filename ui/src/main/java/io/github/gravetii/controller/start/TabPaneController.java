bf47206b38102a8434eeb742cb9d82aeebc3ffa8
Sandeep Dasika <dasika.sandy@gmail.com>
2019-07-27 23:50:53 +0530

    Integrating TabPane in start scene

---
+++ ui/src/main/java/io/github/gravetii/controller/start/TabPaneController.java
@@ -0,0 +1,22 @@
+package io.github.gravetii.controller.start;
+
+import io.github.gravetii.controller.FxController;
+import javafx.fxml.FXML;
+import javafx.scene.Node;
+import javafx.scene.control.Tab;
+import javafx.scene.control.TabPane;
+import javafx.stage.Stage;
+
+public class TabPaneController implements FxController {
+  private Stage stage;
+
+  @FXML private TabPane tabPane;
+
+  public TabPaneController(Stage stage) {
+    this.stage = stage;
+  }
+
+  public void addTab(String title, Node node) {
+    tabPane.getTabs().add(new Tab(title, node));
+  }
+}
--- ui/src/main/java/io/github/gravetii/scene/start/SearchLogScene.java
+++ ui/src/main/java/io/github/gravetii/scene/start/SearchLogScene.java
@@ -1,27 +1,32 @@
 package io.github.gravetii.scene.start;
 
+import io.github.gravetii.controller.start.FileResult;
 import io.github.gravetii.scene.FxScene;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.Region;
 import javafx.stage.Stage;
 
 public class SearchLogScene extends FxScene {
-  private StartScene parent;
   private FileSearchComponent fileSearchComponent;
   private LogComponent logComponent;
+  private ResultListComponent resultListComponent;
+  private TabPaneComponent tabPaneComponent;
 
   protected SearchLogScene(Stage stage, StartScene parent) throws Exception {
     super(stage, new BorderPane());
-    this.parent = parent;
     fileSearchComponent = new FileSearchComponent(parent);
     logComponent = new LogComponent(stage);
+    resultListComponent = new ResultListComponent(stage, parent);
+    tabPaneComponent = new TabPaneComponent(stage);
   }
 
   @Override
   public Region build() {
     BorderPane pane = (BorderPane) root;
     pane.setLeft(fileSearchComponent.getNode());
-    pane.setCenter(logComponent.getNode());
+    tabPaneComponent.addTab("log", logComponent.getNode());
+    tabPaneComponent.addTab("Search", resultListComponent.getNode());
+    pane.setCenter(tabPaneComponent.getNode());
     return pane;
   }
 
@@ -32,5 +37,9 @@ public class SearchLogScene extends FxScene {
 
   public void writeToLog(String text) {
     logComponent.getController().writeToLog(text);
+  }
+
+  public void show(FileResult result) {
+    resultListComponent.getController().show(result);
   }
 }
