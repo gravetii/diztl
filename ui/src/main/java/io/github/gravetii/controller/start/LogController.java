8e46edd348bdddbb1b03440b903af832959113e8
Sandeep Dasika <dasika.sandy@gmail.com>
2019-07-28 02:50:46 +0530

    Adding context menu to log and error log

--- ui/src/main/java/io/github/gravetii/controller/start/LogController.java
+++ ui/src/main/java/io/github/gravetii/controller/start/LogController.java
@@ -2,7 +2,10 @@ package io.github.gravetii.controller.start;
 
 import io.github.gravetii.controller.FxController;
 import javafx.application.Platform;
+import javafx.beans.binding.Bindings;
 import javafx.fxml.FXML;
+import javafx.scene.control.ContextMenu;
+import javafx.scene.control.MenuItem;
 import javafx.scene.control.TextArea;
 
 public class LogController implements FxController {
@@ -20,6 +23,20 @@ public class LogController implements FxController {
     } else {
       logArea.setText("Welcome to Diztl.\n\nPlease wait while we set things up for you...\n\n");
     }
+
+    setContextMenu();
+  }
+
+  private void setContextMenu() {
+    ContextMenu menu = new ContextMenu();
+    MenuItem clearMenuItem = new MenuItem("Clear");
+    clearMenuItem.setOnAction(event -> {
+      logArea.clear();
+    });
+
+    clearMenuItem.disableProperty().bind(Bindings.isEmpty(logArea.textProperty()));
+    menu.getItems().add(clearMenuItem);
+    logArea.setContextMenu(menu);
   }
 
   public void write(String text) {
