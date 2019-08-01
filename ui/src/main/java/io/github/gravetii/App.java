0c116eaf63b9492685f361e0ef0adfede2aadd6c
Sandeep Dasika <dasika.sandy@gmail.com>
2019-08-01 23:51:47 +0530

    Using JFXAlert to display dialog during exit check

--- ui/src/main/java/io/github/gravetii/App.java
+++ ui/src/main/java/io/github/gravetii/App.java
@@ -1,17 +1,22 @@
 package io.github.gravetii;
 
+import com.jfoenix.animation.alert.JFXAlertAnimation;
+import com.jfoenix.controls.JFXAlert;
+import com.jfoenix.controls.JFXButton;
+import com.jfoenix.controls.JFXDialogLayout;
 import io.github.gravetii.client.connection.CommunicationClient;
 import io.github.gravetii.client.handler.ExecutionHandler;
 import io.github.gravetii.scene.start.StartScene;
+import io.github.gravetii.util.Utils;
 import javafx.application.Application;
 import javafx.application.Platform;
-import javafx.scene.control.Alert;
-import javafx.scene.control.ButtonType;
+import javafx.scene.control.ButtonBar;
+import javafx.scene.control.Label;
+import javafx.stage.Modality;
 import javafx.stage.Stage;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import java.util.Optional;
 import java.util.concurrent.Future;
 
 public class App extends Application {
@@ -22,32 +27,38 @@ public class App extends Application {
     launch(args);
   }
 
-  public static boolean close(Stage stage) {
-    if (exitCheck(stage)) {
-      Platform.exit();
-      return true;
-    }
-
-    return false;
-  }
-
-  private static boolean exitCheck(Stage stage) {
-    Alert alert =
-        new Alert(Alert.AlertType.CONFIRMATION, "Are you sure?", ButtonType.NO, ButtonType.YES);
-    alert.setHeaderText("");
-    alert.setTitle("Really Exit?");
-    alert.initOwner(stage);
-    Optional<ButtonType> type = alert.showAndWait();
-    return type.isPresent() && type.get() == ButtonType.YES;
+  private static JFXAlert<Void> getCloseAlert(Stage stage) {
+    JFXDialogLayout layout = new JFXDialogLayout();
+    Utils.addGlobalStylesheet(layout);
+    layout.setHeading(new Label("Really Exit?"));
+    JFXAlert<Void> alert = new JFXAlert<>(stage);
+    alert.setOverlayClose(false);
+    alert.setAnimation(JFXAlertAnimation.CENTER_ANIMATION);
+    JFXButton yes = new JFXButton("Yes");
+    yes.setId("okBtn");
+    yes.setDefaultButton(true);
+    JFXButton no = new JFXButton("No");
+    no.setId("cancelBtn");
+    no.setCancelButton(true);
+    ButtonBar bar = new ButtonBar();
+    bar.getButtons().add(yes);
+    bar.getButtons().add(no);
+    bar.setId("btnBar");
+    layout.setActions(bar);
+    yes.setOnAction(event -> Platform.exit());
+    no.setOnAction(event -> alert.close());
+    alert.setContent(layout);
+    alert.initModality(Modality.APPLICATION_MODAL);
+    return alert;
   }
 
   @Override
   public void start(Stage stage) throws Exception {
     stage.setOnCloseRequest(
         event -> {
-          if (!close(stage)) {
-            event.consume();
-          }
+          JFXAlert<Void> alert = getCloseAlert(stage);
+          alert.show();
+          event.consume();
         });
 
     StartScene scene = new StartScene(stage);
