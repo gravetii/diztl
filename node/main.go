99e0185e7b2644476425e720347eec4f510dbe82
Sandeep Dasika <dasika.sandy@gmail.com>
2019-10-28 18:35:10 +0530

    Adding code for starting UI

--- node/main.go
+++ node/main.go
@@ -3,8 +3,13 @@ package main
 import (
 	"fmt"
 	"net"
+	"os"
 
 	"github.com/gravetii/diztl/addr"
+	"github.com/therecipe/qt/core"
+	"github.com/therecipe/qt/quick"
+	"github.com/therecipe/qt/quickcontrols2"
+	"github.com/therecipe/qt/widgets"
 
 	"github.com/gravetii/diztl/startup"
 
@@ -15,6 +20,42 @@ import (
 	"github.com/gravetii/diztl/service"
 	"github.com/gravetii/logger"
 )
+
+// QmlBridge to pass data back and forth with the frontend.
+type QmlBridge struct {
+	core.QObject
+	_ func()            `constructor:"init"`
+	_ func(term string) `slot:"search"`
+}
+
+func (qmlBridge *QmlBridge) init() {
+	fmt.Println("Initializing QmlBridge...")
+}
+
+func (qmlBridge *QmlBridge) configure() {
+	// connect the search slot whenever the user searches
+	// for files with a query term.
+	qmlBridge.ConnectSearch(func(term string) {
+		fmt.Println("Searching for string: ", term)
+	})
+}
+
+// Start the diztl UI.
+func startUI() {
+	core.QCoreApplication_SetAttribute(core.Qt__AA_EnableHighDpiScaling, true)
+	widgets.NewQApplication(len(os.Args), os.Args)
+	quickcontrols2.QQuickStyle_SetStyle("Material")
+	view := quick.NewQQuickView(nil)
+	view.SetMinimumSize(core.NewQSize2(600, 300))
+	view.SetResizeMode(quick.QQuickView__SizeRootObjectToView)
+	view.SetTitle("Diztl")
+	var qmlBridge = NewQmlBridge(nil)
+	qmlBridge.configure()
+	view.RootContext().SetContextProperty("qmlBridge", qmlBridge)
+	view.SetSource(core.NewQUrl3("./qml/main.qml", 0))
+	view.Show()
+	widgets.QApplication_Exec()
+}
 
 func main() {
 	// Execute the initial startup steps
