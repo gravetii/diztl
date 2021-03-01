75259d030a14d2e777415db1123ce7f005a64df4
Sandeep Dasika <dasika.sandy@gmail.com>
2021-03-01 20:53:59 +0530

    NodeKeeper saves with ip as keys

--- src/main/java/io/github/gravetii/keeper/NodeKeeper.java
+++ src/main/java/io/github/gravetii/keeper/NodeKeeper.java
@@ -10,13 +10,24 @@ import org.slf4j.LoggerFactory;
 import java.util.Map;
 import java.util.UUID;
 import java.util.concurrent.ConcurrentHashMap;
+import java.util.stream.Collectors;
 
 @Singleton
 public class NodeKeeper {
 
+  static class NodeData {
+    private final Node node;
+    private final DiztlConnection connection;
+
+    NodeData(Node node, DiztlConnection connection) {
+      this.node = node;
+      this.connection = connection;
+    }
+  }
+
   private static final Logger logger = LoggerFactory.getLogger(NodeKeeper.class.getCanonicalName());
 
-  private final Map<Node, DiztlConnection> connections = new ConcurrentHashMap<>();
+  private final Map<String, NodeData> connections = new ConcurrentHashMap<>();
 
   public NodeKeeper() {}
 
@@ -27,25 +38,29 @@ public class NodeKeeper {
   }
 
   public DiztlConnection getOrCreate(Node node) {
-    if (connections.containsKey(node)) return connections.get(node);
+    if (connections.containsKey(node.getIp())) return connections.get(node.getIp()).connection;
     DiztlConnection connection = newConnection(node);
-    connections.put(node, connection);
+    connections.put(node.getIp(), new NodeData(node, connection));
     return connection;
   }
 
   public Node register(Node node) {
-    if (connections.containsKey(node)) logger.warn("Node {} already in connections", node);
+    if (connections.containsKey(node.getIp())) {
+      logger.warn("Node {} already in connections", node.getIp());
+      return connections.get(node.getIp()).node;
+    }
+
     UUID uuid = UUID.randomUUID();
     Node result = Node.newBuilder().setId(uuid.toString()).setIp(node.getIp()).build();
     DiztlConnection connection = newConnection(result);
-    connections.put(result, connection);
+    connections.put(result.getIp(), new NodeData(result, connection));
     return result;
   }
 
   public boolean disconnect(Node node) {
-    DiztlConnection connection = connections.remove(node);
-    if (connection != null) {
-      connection.close();
+    NodeData data = connections.remove(node.getIp());
+    if (data != null) {
+      data.connection.close();
       return true;
     }
 
@@ -53,10 +68,10 @@ public class NodeKeeper {
   }
 
   public Map<Node, DiztlConnection> nodes() {
-    return this.connections;
+    return connections.values().stream().collect(Collectors.toMap(x -> x.node, x -> x.connection));
   }
 
   public void close() {
-    connections.forEach((x, y) -> y.close());
+    connections.forEach((x, y) -> y.connection.close());
   }
 }
